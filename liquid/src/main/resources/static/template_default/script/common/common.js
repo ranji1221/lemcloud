f59a9fb2a9b0dc6065fb0dd425bf7d57dd09b582
BOBO <1192012793@qq.com>
2017-10-29 22:55:40 +0800

    删除没用的代码

--- liquid/src/main/resources/static/template_default/script/common/common.js
+++ liquid/src/main/resources/static/template_default/script/common/common.js
@@ -9,7 +9,7 @@
 	 * @param value
 	 * @param storageType
 	 */
-	//把数据保存到缓存中
+	//把数据保持到缓存中
 	function setStorage(key,value,storageType) {
 		var storage = storageType || sessionStorage;
 		var value = JSON.stringify(value);
@@ -158,37 +158,3 @@
 	       }  
 	       return format;  
 	}  
-	//数字加减控制
-	$(function(){
-		//加减数字
-		
-		$(".reduce").click(function(e) {
-
-			e.preventDefault();
-			var numVal = $(".input_num input").val();
-			if(numVal == 1) {
-				$(".add").addClass("active")
-				$(".reduce").removeClass("active")
-			}
-			if(numVal > 0) {
-				//					alert(1)
-				$(".add").addClass("active")
-				numVal--;
-				$(".input_num input").val(numVal);
-			}
-		});
-		$(".add").click(function(e) {
-			e.preventDefault();
-			var numVal = $(".input_num input").val();
-			if(numVal < 10) {
-				numVal++;
-				$(".add").addClass("active")
-				$(".reduce").addClass("active")
-				$(".input_num input").val(numVal);
-			}
-			if(numVal == 10) {
-				$(".reduce").addClass("active")
-				$(".add").removeClass("active")
-			}
-		});
-	})
