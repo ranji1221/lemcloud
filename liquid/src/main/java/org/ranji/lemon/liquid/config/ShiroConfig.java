245ef0bf465acf32c50be9a9a2a5e9d421b9ac6f
jiran1221 <jiran1221@163.com>
2017-11-01 17:31:51 +0800

    1.增加安全框架的校验和异常处理

--- liquid/src/main/java/org/ranji/lemon/liquid/config/ShiroConfig.java
+++ liquid/src/main/java/org/ranji/lemon/liquid/config/ShiroConfig.java
@@ -30,8 +30,9 @@ public class ShiroConfig {
         filterChainDefinitionMap.put("/captcha","anon");
         filterChainDefinitionMap.put("/common/**", "anon"); 
         filterChainDefinitionMap.put("/default/**","anon");
-       // filterChainDefinitionMap.put("/**", "authc");//表示需要认证才可以访问
-        filterChainDefinitionMap.put("/**", "anon");
+        filterChainDefinitionMap.put("/druid/**", "anon");
+        filterChainDefinitionMap.put("/**", "authc");//表示需要认证才可以访问
+       // filterChainDefinitionMap.put("/**", "anon");
         bean.setFilterChainDefinitionMap(filterChainDefinitionMap);
         return bean;
     }
