48b8d8695df858d54276c411e3b241a997873243
jiran1221 <jiran1221@163.com>
2017-11-03 12:33:09 +0800

    1.删除冗余的代码

--- liquid/src/main/java/org/ranji/lemon/liquid/service/authority/impl/UserServiceImpl.java
+++ liquid/src/main/java/org/ranji/lemon/liquid/service/authority/impl/UserServiceImpl.java
@@ -11,7 +11,6 @@ import org.ranji.lemon.liquid.model.authority.User;
 import org.ranji.lemon.liquid.persist.authority.prototype.IUserDao;
 import org.ranji.lemon.liquid.service.authority.prototype.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
