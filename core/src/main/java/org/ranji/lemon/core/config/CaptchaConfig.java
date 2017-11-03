79fb6acd4f59fad6e31e4c13c1f1d8105b9ee032
jiran1221 <jiran1221@163.com>
2017-11-03 19:51:08 +0800

    1.添加必要的注释

--- core/src/main/java/org/ranji/lemon/core/config/CaptchaConfig.java
+++ core/src/main/java/org/ranji/lemon/core/config/CaptchaConfig.java
@@ -8,6 +8,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import com.google.code.kaptcha.impl.DefaultKaptcha;
 import com.google.code.kaptcha.util.Config;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the"License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Copyright [2017] [RanJi] [Email-jiran1221@163.com]
+ * 
+ * 验证码生成配置
+ * @author RanJi
+ * @date 2017-10-13
+ * @since JDK1.7
+ * @version 1.0
+ */
 
 @Configuration
 public class CaptchaConfig {
--- core/src/main/java/org/ranji/lemon/core/config/TransactionConfig.java
+++ core/src/main/java/org/ranji/lemon/core/config/TransactionConfig.java
@@ -8,7 +8,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the"License"); 
+ * you may not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at
 
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and limitations under the License.
+ * Copyright [2017] [RanJi] [Email-jiran1221@163.com]
+ * 
+ * 事务配置
+ * @author RanJi
+ * @date 2017-10-13
+ * @since JDK1.7
+ * @version 1.0
+ */
 @Configuration
 public class TransactionConfig {
 	@Autowired
